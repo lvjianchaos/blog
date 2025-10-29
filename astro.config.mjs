68e8b5c78a5d385d0668e64079cd77b5aa5ffc61
lvjianchaos <2277146577@qq.com>
2025-10-30 00:40:43 +0800

    repair: add my Custom Domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://chaos-ljc.top/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
